b769f2c86b4888b65337a36555ead2e03a617608
Najmus Sakib Dhrubo <sakib.ngdt@gmail.com>
2023-08-13 18:07:46 +0600

    Develop Why us cont.

--- core-bangla-website/src/pages/Home/components/WhyUs.jsx
+++ core-bangla-website/src/pages/Home/components/WhyUs.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import { VBox, HBox } from "../../../components/Containers";
 import { H3, P3 } from "../../../components/Typography";
+import colors from '../../../config/colors';
 
 const Container = styled(VBox)`
     margin-top: 120px;
@@ -15,13 +16,64 @@ const Underline = styled.div`
     /* Write your CSS */
 `
 
-const LeftShape = styled(VBox)`
-    height: 150px;
-    width: 300px;
-    border: 4px solid black;
+const Item = styled(VBox)`
+    border: 4px solid ${colors.darkGreen};
+    position: relative;
+    /* background-color: blue; */
+    height: 25%;
     border-radius: 25% 25% 25% 25% / 50% 50% 50% 50%;
-    background-color: white;
-    clip-path: polygon(0% 0%, 100% 0%, 100% 50%, 50% 100%, 0 100%);
+`
+
+const Item1 = styled(Item)`
+    clip-path: polygon(0 0, 100% 0, 100% 50%, 90% 50%, 90% 70%, 80% 70%, 80% 90%, 70% 90%, 50% 90%, 50% 100%, 0 100%);
+    top: ${props => props.topPosition};
+    /* background-color: white;
+    height: 100%; */
+`
+
+const Item2 = styled(Item)`
+    clip-path: polygon(50% 0, 50% 15%, 10% 15%, 10% 85%, 50% 85%, 50% 100%, 100% 100%, 100% 0);
+    top: ${props => props.topPosition};
+`
+
+const Item3 = styled(Item)`
+    clip-path: polygon(0 0, 50% 0, 50% 15%, 80% 15%, 80% 30%, 95% 30%, 95% 50%, 100% 50%, 100% 100%, 0 100%);
+    top: ${props => props.topPosition};
+`
+
+
+const CircleContainer = styled(HBox)`
+    /* border: 3px solid black; */
+    background-color: ${colors.white};
+    position: absolute;
+    width: 20%;
+    top: ${props => props.topPosition};
+    left: ${props => props.leftPosition};
+    z-index: 1;
+`
+
+const Circle = styled(HBox)`
+    border: 4px solid ${colors.darkGreen};
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+`
+
+const CenterCircleContainer = styled(VBox)`
+    border: 3px solid blue;
+    height: 80%;
+    margin-top: 10%;
+    border-radius: 50%;
+    background-image: linear-gradient(to top right, ${colors.veryLightGrey}, #EEEEEE);
+    padding: 20px;
+`
+
+const CenterCircle = styled(VBox)`
+    border: 3px solid green;
+    height: 100%;
+    /* margin-top: 10%; */
+    border-radius: 50%;
+    background-image: linear-gradient(to top right, ${colors.veryLightGrey}, ${colors.grey}); 
 `
 
 const WhyUs = () => {
@@ -29,18 +81,66 @@ const WhyUs = () => {
         <Container align='center'>
             <H3 className="bold">কেন আমদের প্লাটফর্ম ব্যবহার করবেন?</H3>
             <Underline className="my-8" />
-            <HBox justify='center'>
-                <VBox>
+            <HBox justify='center' style={{height: '40rem', width: '100%', border: '3px solid black'}}>
+                <VBox justify='center' style={{width: '25%', position: 'relative'}}>
                     {/* Left */}
-                    <LeftShape align='center' justify='center'>
-                        <P3>নিবন্ধিত বিশেষজ্ঞ চিকিৎসক</P3>
-                    </LeftShape>
+                    <Item1 className='m-2' topPosition='0' justify='center' align='center'>
+                        <P3 align='center'>নিবন্ধিত বিশেষজ্ঞ চিকিৎসক</P3>
+                    </Item1>
+                    
+                    <CircleContainer justify='center' topPosition='33.8%' leftPosition='41%'>
+                        <Circle></Circle>
+                    </CircleContainer>
+                    
+                    <Item2 className='mb-2 ml-2 mr-2' topPosition='-20px' justify='center' align='center'>
+                        <P3 align='center'>ই-প্রেসক্রিপশন সেবা</P3>
+                    </Item2>
+
+                    <CircleContainer justify='center' topPosition='58%' leftPosition='41%'>
+                        <Circle></Circle>
+                    </CircleContainer>
+
+                    <Item3 className='ml-2 mr-2' topPosition='-40px' justify='center' align='center'>
+                        <P3 align='center'>ডায়গনিস্টিক রিপোর্ট আপলোড</P3>
+                    </Item3>
                 </VBox>
-                <VBox >
+
+
+                <VBox style={{width: '50%'}}>
                     {/* Center */}
+                    <VBox style={{border: '3px solid red', height: '100%', padding: '40px'}}>
+                        <CenterCircleContainer>
+                            <CenterCircle>
+
+                            </CenterCircle>
+                        </CenterCircleContainer>
+                    </VBox>
                 </VBox>
-                <VBox>
+
+
+                <VBox justify='center' style={{width: '25%', position: 'relative'}}>
                     {/* Right */}
+
+                    <Item1 className='m-2' topPosition='0' justify='center' align='center' style={{transform: 'ScaleX(-1)'}}>
+                        <P3 align='center' style={{transform: 'ScaleX(-1)'}}>ভিডিও এবং অডিও পরামর্শ</P3>
+                    </Item1>
+
+                    <CircleContainer justify='center' topPosition='33.8%' leftPosition='41%'>
+                        <Circle></Circle>
+                    </CircleContainer>
+
+                    <Item2 className='mb-2 ml-2 mr-2' topPosition='-20px' justify='center' align='center' style={{transform: 'ScaleX(-1)'}}>
+                        <P3 align='center' style={{transform: 'ScaleX(-1)'}}>অনলাইন এবং অফলাইন</P3>
+                        <P3 align='center' style={{transform: 'ScaleX(-1)'}}>পরামর্শ</P3>
+                    </Item2>
+
+                    <CircleContainer justify='center' topPosition='58%' leftPosition='41%'>
+                        <Circle></Circle>
+                    </CircleContainer>
+
+                    <Item3 className='ml-2 mr-2' topPosition='-40px' justify='center' align='center' style={{transform: 'ScaleX(-1)'}}>
+                        <P3 align='center' style={{transform: 'ScaleX(-1)'}}>ক্লাউড-ভিত্তিক ডেটা এবং গোপনীয়তার নিরাপত্তা</P3>
+                    </Item3>
                 </VBox>
             </HBox>
         </Container>
