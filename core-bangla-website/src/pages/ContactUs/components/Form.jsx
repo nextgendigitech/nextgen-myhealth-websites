f46f9a987b0768113221e19d52d39853d0eb25bf
Zulkar Nayem <nayem.ngdt@gmail.com>
2023-08-13 12:55:16 +0600

    Added boilerplates for contact us page.

---
+++ core-bangla-website/src/pages/ContactUs/components/Form.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+import { VBox } from "../../../components/Containers";
+
+const Container = styled(VBox)`
+
+`
+
+const Form = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+}
+
+export default Form;
--- core-bangla-website/src/pages/ContactUs/index.jsx
+++ core-bangla-website/src/pages/ContactUs/index.jsx
@@ -1,10 +1,12 @@
 import { VBox } from "../../components/Containers";
-import { H1 } from "../../components/Typography";
+import Map from "./components/Map";
+import Form from "./components/Form";
 
 const ContactUs = () => {
     return (
         <VBox>
-            <H1>Contact Us Page</H1>
+            <Map />
+            <Form />
         </VBox>
     );
 }
