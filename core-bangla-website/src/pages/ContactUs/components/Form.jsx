6a488890d6fb8c130a842f8f15d9d84919248817
Ramisa Fariha Joyee <107878401+ramisafarihajoyee@users.noreply.github.com>
2023-08-14 16:46:19 +0600

    Modified Contact us page

--- core-bangla-website/src/pages/ContactUs/components/Form.jsx
+++ core-bangla-website/src/pages/ContactUs/components/Form.jsx
@@ -1,20 +1,19 @@
 import styled from "styled-components";
-
-import { Button } from '../../../components/Buttons';
 import colors from "../../../config/colors";
+
+import { Button } from "../../../components/Buttons";
 import { HBox, VBox } from "../../../components/Containers";
-import { H5, P2, P4 } from "../../../components/Typography";
+import { H5, P2, P3 } from "../../../components/Typography";
+import { HiOutlinePhone } from "react-icons/hi"
+import { AiOutlineMail } from "react-icons/ai"
 
 
-const VContainer = styled(VBox)`
-
-`
 const Card = styled.div`
     width: 800px;
     border: 1px solid ${colors.white};
     padding: 25px;
     border-radius: 8px;
-    box-shadow: 5px 4px 5px ${colors.grey};
+    box-shadow: 2px 5px 4px 5px ${colors.grey};
 `;
 
 const FillupForm = styled.form`
@@ -25,62 +24,72 @@ const FillupForm = styled.form`
 const Input = styled.input`
     margin-bottom: 20px;
     padding: 8px;
-    border: 1px solid #ccc;
+    border: 1px solid ${colors.green};
     border-radius: 5px;
-
     width: 360px;
     height: 40px;
     padding: 8px;
 `;
 
-const UnderlinedText = styled.span`
-  border-bottom: 2px solid ${colors.darkGreen}; 
-  position: relative;
-  left: 295px;
-  top: 17px;
-  width: 20%;
-  margin-bottom: 40px;
+const Underline = styled.span`
+    border-bottom: 2px solid ${colors.darkGreen}; 
+    position: relative;
+    left: 295px;
+    top: 17px;
+    width: 20%;
+    margin-bottom: 50px;
 `;
 
 const SButton = styled(Button)`
-   height: 40px;
-   border-radius: 8px;
-   font-weight: bold;
-   background-color: ${colors.darkGreen};
-   color: ${colors. white};
+    height: 40px;
+    border-radius: 8px;
+    font-weight: bold;
+    /* background-color: ${colors.darkGreen}; */
+    /* color: ${colors. white}; */
 `;
 
 const Form = () => {
+    
     return (
-        <VContainer>
+        <VBox justify="center" align="center">
             <Card>
                 <VBox>
-                    <H5 align='center' color="third">যোগাযোগ করুন</H5>
-                    <UnderlinedText/>
+                    <H5 align="center" color="third">যোগাযোগ করুন</H5>
+                    <Underline/>
                 </VBox>
-                <HBox justify='space-between' align='center'>
+                <HBox justify="space-around" align="center">
                     <VBox>
                         <FillupForm>
-                            <P2 className='mb-2' color="third">বার্তা লিখুন</P2>
-                            <Input type="text" placeholder="নাম" required/>
-                            <Input type="email" placeholder="ই-মেইল" />
-                            <Input type="text" placeholder="বার্তা (প্রশ্নের বিবরণ)" required/>
-                            <SButton type="submit">বার্তা পাঠান</SButton>
+                            <P2 className="bold mb-2" color="third">বার্তা লিখুন</P2>
+                            <Input style={{ paddingLeft: "20px" }} type="text" placeholder="নাম" required/>
+                            <Input style={{ paddingLeft: "20px" }} type="email" placeholder="ই-মেইল" />
+                            <textarea 
+                                style={{ border: "1px solid green", borderRadius: "5px" , paddingLeft: "20px", paddingTop: "10px" }} 
+                                placeholder="বার্তা (প্রশ্নের বিবরণ)" 
+                                rows={8} required>
+                            </textarea>
+                            <SButton type="submit" color="third" className="mt-5" elevated>বার্তা পাঠান</SButton>
                         </FillupForm>
                     </VBox>
                     <VBox>
-                    নেক্সটজেন মাইহেলথ ভিসিপি
-                    স্বাস্থ্য বিভাগ
-                    নেক্সটজেন ডিজিটেক লিমিটেড
-                    টুল অ্যান্ড টেকনোলজি ইনস্টিটিউট, বিট্যাক শিল্প মন্ত্রণালয় ১১৬ (খ), তেজগাঁও শিল্প এলাকা, ঢাকা-১২০৮, বাংলাদেশ
-
-                    +৮৮০ ১৩২১১১৯৩৯১
-
-                    info@nextgenmyhealth.com
+                        <P2 className="bold">নেক্সটজেন মাইহেলথ ভিসিপি</P2>
+                        <P3>স্বাস্থ্য বিভাগ</P3>
+                        <P3 color="first">নেক্সটজেন ডিজিটেক লিমিটেড</P3>
+                        <P3>টুল অ্যান্ড টেকনোলজি ইনস্টিটিউট, বিট্যাক শিল্প</P3>
+                        <P3>মন্ত্রণালয় ১১৬ (খ), তেজগাঁও শিল্প এলাকা,</P3>
+                        <P3>ঢাকা-১২০৮, বাংলাদেশ</P3>
+                        <HBox className="mt-3">
+                            <HiOutlinePhone/>
+                            <P3>+৮৮০১৩২১১১৯৩৯১</P3>
+                        </HBox>
+                        <HBox className="mt-3 mb-6">
+                            <AiOutlineMail/>
+                            <P3>info@nextgenmyhealth.com</P3>
+                        </HBox>
                     </VBox>
                 </HBox>
             </Card>
-        </VContainer>        
+        </VBox>        
     );
 }
 
--- core-bangla-website/src/pages/ContactUs/components/Map.jsx
+++ core-bangla-website/src/pages/ContactUs/components/Map.jsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
 
 import { HBox } from '../../../components/Containers';
-import { Button } from '../../../components/Buttons';
-import colors from "../../../config/colors";
+
+const Container = styled(HBox)`
+    margin-bottom: 120px;
+`
 
 const Map = () => {
     return (
-        <>
-        </>
+        <Container>
+            <iframe style={{ width: "100%", height: "500px" }} 
+                src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=Tool%20%26%20Technology%20Institute%20(TTI)%2C%20BITAC%20-%20%E0%A6%9F%E0%A7%81%E0%A6%B2%20%E0%A6%8F%E0%A6%A8%E0%A7%8D%E0%A6%A1%20%E0%A6%9F%E0%A7%87%E0%A6%95%E0%A6%A8%E0%A7%8B%E0%A6%B2%E0%A6%9C%E0%A6%BF%20%E0%A6%87%E0%A6%A8%E0%A6%B8%E0%A7%8D%E0%A6%9F%E0%A6%BF%E0%A6%9F%E0%A6%BF%E0%A6%89%E0%A6%9F%20(%E0%A6%9F%E0%A6%BF%E0%A6%9F%E0%A6%BF%E0%A6%86%E0%A6%87)%2C%20%E0%A6%AC%E0%A6%BF%E0%A6%9F%E0%A6%BE%E0%A6%95%2C%20Dhaka%201208+(Tool%20%26%20Technology%20Institute%20(TTI)%2C%20BITAC%20-%20%E0%A6%9F%E0%A7%81%E0%A6%B2%20%E0%A6%8F%E0%A6%A8%E0%A7%8D%E0%A6%A1%20%E0%A6%9F%E0%A7%87%E0%A6%95%E0%A6%A8%E0%A7%8B%E0%A6%B2%E0%A6%9C%E0%A6%BF%20%E0%A6%87%E0%A6%A8%E0%A6%B8%E0%A7%8D%E0%A6%9F%E0%A6%BF%E0%A6%9F%E0%A6%BF%E0%A6%89%E0%A6%9F%20(%E0%A6%9F%E0%A6%BF%E0%A6%9F%E0%A6%BF%E0%A6%86%E0%A6%87)%2C%20%E0%A6%AC%E0%A6%BF%E0%A6%9F%E0%A6%BE%E0%A6%95%2C%20Dhaka%201208)&amp;ie=UTF8&amp;t=&amp;z=17&amp;iwloc=B&amp;output=embed">
+            </iframe>
+        </Container>
     );
 }
 
