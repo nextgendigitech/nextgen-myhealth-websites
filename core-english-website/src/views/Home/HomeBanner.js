eeef83b58b33e8eeb1522a9a0049984686248dbc
Najmus Sakib Dhrubo <sakib.ngdt@gmail.com>
2023-08-21 18:03:02 +0600

    Added home banner and updated specialities

--- core-english-website/src/views/Home/HomeBanner.js
+++ core-english-website/src/views/Home/HomeBanner.js
@@ -1,7 +1,8 @@
 import { Box, Grid, Slide, Typography, useMediaQuery, useTheme } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import SliderOne from '../../images/home-banner.png';
-import SliderTwo from '../../images/home-banner-1.png';
+import SliderOne from '../../images/web-banner.png';
+import SliderTwo from '../../images/home-banner.png';
+import SliderThree from '../../images/home-banner-1.png';
 import { styled } from '@mui/system';
 import '../../App.css'
 
@@ -17,7 +18,7 @@ const HomeBanner = () => {
   const isMediumScreen = useMediaQuery(useTheme().breakpoints.down('md'));
   const isMobileScreen = useMediaQuery(useTheme().breakpoints.down('sm'));
 
-  const sliderList = [SliderOne, SliderTwo]
+  const sliderList = [SliderOne, SliderTwo, SliderThree]
 
   const [bannerIndex, setBannerIndex] = useState(0)
   const [slideIn, setSlideIn] = useState(false)
