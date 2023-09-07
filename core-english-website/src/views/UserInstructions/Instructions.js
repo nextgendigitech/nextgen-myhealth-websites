6c98ea4084821a2fe51434a6fc5b1a6c2521f94c
Najmus Sakib Dhrubo <sakib.ngdt@gmail.com>
2023-09-07 15:18:43 +0600

    Developed user instructions

--- core-english-website/src/views/UserInstructions/Instructions.js
+++ core-english-website/src/views/UserInstructions/Instructions.js
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import styled from 'styled-components';
 
-import { H2 } from '../../components/Typography';
+import { H1, H2 } from '../../components/Typography';
 import colors from '../../utility/styles';
 import responsive from '../../utility/responsive';
 import logo from '../../images/google-play-store-logo.svg';
@@ -12,7 +12,6 @@ const Container = styled(Box)`
     flex-direction: row;
     background-color: ${colors.primaryLight};
     overflow: hidden;
-    border: 3px solid orange;
 `
 
 const InstructionContainerOuter = styled(Box)`
@@ -23,7 +22,6 @@ const InstructionContainerOuter = styled(Box)`
     width: 100%;
     justify-content: space-around;
     align-items: center;
-    border: 3px solid red;
     flex-wrap: wrap;
 `
 
@@ -35,11 +33,20 @@ const InstructionContainerInner = styled(Box)`
     height: 500px;
     width: 500px;
     min-width: 250px;
-    border: 3px solid black;
 
     @media only screen and (max-width: ${responsive.mobileThresh}px) {
         width: 100%;
     }
+`
+
+const ManualContainer = styled(Box)`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    height: 250px;
+    width: 350px;
+    background-color: ${colors.lightBlue};
 `
 
 const Circle = styled(Box)`
@@ -56,29 +63,31 @@ const Instruction = () => {
     return (
         <Container>
             <InstructionContainerOuter>
-                <Circle top='-30vw' left='-10vw' height='700px' width='700px' />
-                <Circle top='100px' left='-70px' height='150px' width='150px' backgroundColor={colors.whiteColor} style={{boxShadow: `20px 20px 120px 1px ${colors.greyColor}`}} />
-                <Circle top='350px' left='-100px' height='250px' width='250px' backgroundColor={colors.whiteColor} style={{border: `30px solid ${colors.primaryDark}`}} />
-                <Circle top='-50px' left='700px' backgroundColor={colors.primaryDark} style={{boxShadow: `20px 20px 120px 1px ${colors.greyColor}`}} />
-                <Circle top='-100px' left='1300px' height='600px' width='600px'/>
-                <Circle top='-50px' left='1300px' height='100px' width='100px' backgroundColor={colors.whiteColor} style={{boxShadow: `20px 20px 120px 1px ${colors.greyColor}`}} />
-                <Circle top='440px' left='1400px' backgroundColor={colors.primaryDark} />
+                <Circle top='-90%' left='-20%' height='700px' width='700px' />
+                <Circle top='25%' left='-5%' height='150px' width='150px' backgroundColor={colors.whiteColor} style={{boxShadow: `5px 5px 30px 1px ${colors.greyColor}`}} />
+                <Circle top='75%' left='-5%' height='250px' width='250px' backgroundColor={colors.whiteColor} style={{border: `30px solid ${colors.primaryDark}`}} />
+                <Circle top='-10%' left='48%' backgroundColor={colors.primaryDark} style={{boxShadow: `5px 5px 30px 1px ${colors.greyColor}`}} />
+                <Circle top='-10%' left='90%' height='600px' width='600px'/>
+                <Circle top='-5%' left='90%' height='100px' width='100px' backgroundColor={colors.whiteColor} style={{boxShadow: `5px 5px 30px 1px ${colors.greyColor}`}} />
+                <Circle top='88%' left='95%' backgroundColor={colors.primaryDark} />
 
                 <InstructionContainerInner className='pt-5 pl-5 pr-5'>
-                    <Box style={{backgroundColor: `${colors.primaryColor}`, width: '90%'}}>
-                        <H2 align='center' color='white'>
-                            DOWNLOAD PATIENT APP
-                        </H2>
-                    </Box>
+                    <H2 className='bold' align='center' color='black'>
+                        DOWNLOAD PATIENT APP
+                    </H2>
                     <img className='mt-5' src={logo}/>
                     <img className='mt-5' src={qr_code} style={{height: '150px', width: '150px'}} />
                 </InstructionContainerInner>
                 <InstructionContainerInner className='pt-5 pl-5 pr-5'>
-                    <Box style={{backgroundColor: `${colors.primaryColor}`, width: '90%'}}>
-                        <H2 align='center' color='white'>
-                            ARE YOU A DOCTOR?
-                        </H2>
-                    </Box>
+                    <H2 className='bold' align='center' color='black'>
+                        ARE YOU A DOCTOR?
+                    </H2>
+
+                    <ManualContainer className='mt-8'>
+                        <H1 className='bold' color='black'>
+                            MANUAL
+                        </H1>
+                    </ManualContainer>
                 </InstructionContainerInner>
             </InstructionContainerOuter>
         </Container>
